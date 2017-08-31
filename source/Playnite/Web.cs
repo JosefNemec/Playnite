b6a3e55163b5d672dd12b11eb1177fe0f99499b5
Josef Nemec <nemecjosef@gmail.com>
2017-08-31 17:32:59 +0200

    Fix: Crash when downloading metadata

--- source/Playnite/Web.cs
+++ source/Playnite/Web.cs
@@ -31,6 +31,7 @@ namespace Playnite
 
         public static void DownloadFile(string url, string path)
         {
+            FileSystem.CreateFolder(Path.GetDirectoryName(path));
             var webClient = new WebClient();
             webClient.DownloadFile(url, path);
         }
