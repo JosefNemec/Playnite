00285040bb3e22077006f3078d3c8ace372e595f
Josef Nemec <nemecjosef@gmail.com>
2018-04-28 20:30:49 +0200

    Simple script to import play time from raptr DB export

---
+++ scripts/Extensions/RaptrTimeImporter.ps1
@@ -0,0 +1,43 @@
+$global:__attributes = @{
+    "Author" = "Josef Nemec";
+    "Version" = "1.0"
+}
+
+$global:__exports = @(
+    @{
+        "Name" = "Import Raptr Play Times";
+        "Function" = "ImportRaptr"
+    }
+)
+
+# Imports time from Raptr database export
+function global:ImportRaptr()
+{
+    if (!($raptrPath = $PlayniteApi.Dialogs.SelectFile("*.json|*.json")))
+    {
+        return
+    }
+
+    $raptrGames = Get-Content $raptrPath -Raw | ConvertFrom-Json
+    $raptrGames | ForEach {
+        $_.title = $_.title -replace "\s\(.+?\)$", ""
+    }
+
+    $updateCount = 0
+    $games = $PlayniteApi.Database.GetGames()
+    :mainLoop foreach ($game in $games)
+    {
+        foreach ($raptrGame in $raptrGames)
+        {
+            if ($game.Name -eq $raptrGame.title)
+            {
+                $game.PlayTime = $raptrGame.total_playtime_seconds
+                $PlayniteApi.Database.UpdateGame($game)
+                $updateCount++
+                continue :mainLoop
+            }
+        }
+    }
+
+    $PlayniteApi.Dialogs.ShowMessage("Imported play time for $updateCount games.")
+}
