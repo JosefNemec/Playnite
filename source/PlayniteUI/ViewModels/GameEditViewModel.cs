a36ffbf9b3abf9424696d6fa9295e079352f44d9
Josef Nemec <nemecjosef@gmail.com>
2018-05-02 11:06:24 +0200

    New: Ability to manually set installation state for custom games #546

--- source/PlayniteUI/ViewModels/GameEditViewModel.cs
+++ source/PlayniteUI/ViewModels/GameEditViewModel.cs
@@ -1968,24 +1968,10 @@ namespace PlayniteUI.ViewModels
                     }
                 }
 
-                if (EditingGame.Provider == Provider.Custom &&
-                    string.IsNullOrEmpty(EditingGame.IsoPath) &&
-                    string.IsNullOrEmpty(EditingGame.InstallDirectory))
+                if (Game.State.Installed != EditingGame.State.Installed)
                 {
-                    // For UWP games which don't have installed dir
-                    if (EditingGame.PlayTask?.Path == "explorer.exe")
-                    {
-                        Game.State = new GameState(Game.State) { Installed = true };
-                    }
-                    else
-                    {
-                        // GameState is not observable so we need to change the whole object to send notify messages
-                        Game.State = new GameState(Game.State) { Installed = false };
-                    }
-                }
-                else
-                {
-                    Game.State = new GameState(Game.State) { Installed = true };
+                    // GameState is not observable so we need to change the whole object to send notify messages
+                    Game.State = new GameState(Game.State) { Installed = EditingGame.State.Installed };
                 }
             }
 
