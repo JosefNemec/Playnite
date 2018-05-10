5d7239dae2c17356d97efc740e5382aa7933b111
Josef Nemec <nemecjosef@gmail.com>
2018-05-10 17:39:47 +0200

    Release 4.2

--- source/PlayniteUI/Properties/AssemblyInfo.cs
+++ source/PlayniteUI/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyVersion("4.2.0.0")]
+[assembly: AssemblyFileVersion("4.2.0.0")]
