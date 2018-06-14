27e6ec8787d8cb1a33ae6dc0ce8d0c7af94e7e06
Brett Sanderson <l-brett-l@hotmail.com>
2018-06-13 23:23:48 -0400

    jitter fix

--- source/PlayniteUI/Controls/WindowBase.cs
+++ source/PlayniteUI/Controls/WindowBase.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+using ControlzEx.Behaviors;
 
 namespace PlayniteUI.Controls
 {
@@ -94,6 +93,105 @@ namespace PlayniteUI.Controls
             {
                 Style = defaultStyle;
             }
+            this.InitializeBehaviors();
+        }
+
+        private void InitializeBehaviors()
+        {
+            this.InitializeWindowChromeBehavior();
+            this.InitializeGlowWindowBehavior();
+        }
+
+        /// <summary>
+        /// Initializes the WindowChromeBehavior which is needed to render the custom WindowChrome.
+        /// </summary>
+        private void InitializeWindowChromeBehavior()
+        {
+            var behavior = new WindowChromeBehavior();
+            BindingOperations.SetBinding(behavior, WindowChromeBehavior.TryToBeFlickerFreeProperty, new Binding { Path = new PropertyPath(TryToBeFlickerFreeProperty), Source = this });
+            BindingOperations.SetBinding(behavior, WindowChromeBehavior.ResizeBorderThicknessProperty, new Binding { Path = new PropertyPath(ResizeBorderThicknessProperty), Source = this });
+            BindingOperations.SetBinding(behavior, WindowChromeBehavior.IgnoreTaskbarOnMaximizeProperty, new Binding { Path = new PropertyPath(IgnoreTaskbarOnMaximizeProperty), Source = this });
+            BindingOperations.SetBinding(behavior, WindowChromeBehavior.GlowBrushProperty, new Binding { Path = new PropertyPath(GlowBrushProperty), Source = this });
+            BindingOperations.SetBinding(behavior, WindowChromeBehavior.CaptionHeightProperty, new Binding { Path = new PropertyPath(CaptionHeightProperty), Source = this });
+
+            Interaction.GetBehaviors(this).Add(behavior);
+        }
+
+        /// <summary>
+        /// Initializes the WindowChromeBehavior which is needed to render the custom WindowChrome.
+        /// </summary>
+        private void InitializeGlowWindowBehavior()
+        {
+            var behavior = new GlowWindowBehavior();
+            BindingOperations.SetBinding(behavior, GlowWindowBehavior.ResizeBorderThicknessProperty, new Binding { Path = new PropertyPath(ResizeBorderThicknessProperty), Source = this });
+            BindingOperations.SetBinding(behavior, GlowWindowBehavior.GlowBrushProperty, new Binding { Path = new PropertyPath(GlowBrushProperty), Source = this });
+            BindingOperations.SetBinding(behavior, GlowWindowBehavior.NonActiveGlowBrushProperty, new Binding { Path = new PropertyPath(NonActiveGlowBrushProperty), Source = this });
+
+            Interaction.GetBehaviors(this).Add(behavior);
+        }
+
+        public static readonly DependencyProperty TryToBeFlickerFreeProperty = DependencyProperty.Register(nameof(TryToBeFlickerFree), typeof(bool), typeof(WindowBase), new PropertyMetadata(false));
+
+        public bool TryToBeFlickerFree
+        {
+            get { return (bool)this.GetValue(TryToBeFlickerFreeProperty); }
+            set { this.SetValue(TryToBeFlickerFreeProperty, value); }
+        }
+
+        public static readonly DependencyProperty CaptionHeightProperty = DependencyProperty.Register(nameof(CaptionHeight), typeof(double), typeof(WindowBase), new PropertyMetadata(WindowChromeBehavior.CaptionHeightProperty.DefaultMetadata.DefaultValue));
+
+        /// <summary>The extent of the top of the window to treat as the caption.</summary>
+        public double CaptionHeight
+        {
+            get { return (double)this.GetValue(CaptionHeightProperty); }
+            set { this.SetValue(CaptionHeightProperty, value); }
+        }
+
+
+        public Thickness ResizeBorderThickness
+        {
+            get { return (Thickness)this.GetValue(ResizeBorderThicknessProperty); }
+            set { this.SetValue(ResizeBorderThicknessProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ResizeBorderThickness.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ResizeBorderThicknessProperty =
+            DependencyProperty.Register(nameof(ResizeBorderThickness), typeof(Thickness), typeof(WindowBase), new PropertyMetadata(WindowChromeBehavior.ResizeBorderThicknessProperty.DefaultMetadata.DefaultValue));
+
+        public static readonly DependencyProperty IgnoreTaskbarOnMaximizeProperty = DependencyProperty.Register(nameof(IgnoreTaskbarOnMaximize), typeof(bool), typeof(WindowBase), new PropertyMetadata(WindowChromeBehavior.IgnoreTaskbarOnMaximizeProperty.DefaultMetadata.DefaultValue));
+
+        public bool IgnoreTaskbarOnMaximize
+        {
+            get { return (bool)this.GetValue(IgnoreTaskbarOnMaximizeProperty); }
+            set { this.SetValue(IgnoreTaskbarOnMaximizeProperty, value); }
+        }
+
+        /// <summary>
+        /// <see cref="DependencyProperty"/> for <see cref="GlowBrush"/>.
+        /// </summary>
+        public static readonly DependencyProperty GlowBrushProperty = DependencyProperty.Register(nameof(GlowBrush), typeof(Brush), typeof(WindowBase), new PropertyMetadata(default(Brush)));
+
+        /// <summary>
+        /// Gets or sets a brush which is used as the glow when the window is active.
+        /// </summary>
+        public Brush GlowBrush
+        {
+            get { return (Brush)this.GetValue(GlowBrushProperty); }
+            set { this.SetValue(GlowBrushProperty, value); }
+        }
+
+        /// <summary>
+        /// <see cref="DependencyProperty"/> for <see cref="NonActiveGlowBrush"/>.
+        /// </summary>
+        public static readonly DependencyProperty NonActiveGlowBrushProperty = DependencyProperty.Register(nameof(NonActiveGlowBrush), typeof(Brush), typeof(WindowBase), new PropertyMetadata(default(Brush)));
+
+        /// <summary>
+        /// Gets or sets a brush which is used as the glow when the window is not active.
+        /// </summary>
+        public Brush NonActiveGlowBrush
+        {
+            get { return (Brush)this.GetValue(NonActiveGlowBrushProperty); }
+            set { this.SetValue(NonActiveGlowBrushProperty, value); }
         }
 
         public override void OnApplyTemplate()
