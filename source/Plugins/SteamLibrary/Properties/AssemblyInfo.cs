e4e3a523e95b84852f25163a35582f991b175279
Josef Nemec <nemecjosef@gmail.com>
2018-08-28 13:53:59 +0200

    Plugin assembly info fix

--- source/Plugins/SteamLibrary/Properties/AssemblyInfo.cs
+++ source/Plugins/SteamLibrary/Properties/AssemblyInfo.cs
@@ -33,5 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.*")]
-
 [assembly: InternalsVisibleTo("SteamLibrary.Tests")]
--- source/Plugins/UplayLibrary/Properties/AssemblyInfo.cs
+++ source/Plugins/UplayLibrary/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UplayLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Uplay library support plugin for Playnite")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UplayLibrary")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Josef Nemec 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: InternalsVisibleTo("UplayLibrary.Tests")]
