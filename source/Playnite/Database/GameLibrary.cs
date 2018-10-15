bcd9902e12eda5259f087de860471a926b511d30
Josef Nemec <nemecjosef@gmail.com>
2018-10-15 09:08:34 -0700

    Game media can be assigned by a plugin without metadata download

--- source/Playnite/Database/GameLibrary.cs
+++ source/Playnite/Database/GameLibrary.cs
@@ -1,9 +1,11 @@
 ﻿using Playnite.SDK;
 using Playnite.SDK.Models;
 using Playnite.SDK.Plugins;
+using Playnite.Web;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +16,53 @@ namespace Playnite.Database
     {
         private static ILogger logger = LogManager.GetLogger();
 
+        private static string AddNewFile(string path, int gameId, GameDatabase database)
+        {
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(path);
+            var fileId = $"{gameId.ToString()}/{fileName}";
+
+            if (path.StartsWith("http", StringComparison.OrdinalIgnoreCase))
+            {
+                database.AddFile(fileId, fileName, HttpDownloader.DownloadData(path));
+                return fileId;
+            }
+
+            try
+            {
+                if (File.Exists(path))
+                {
+                    if (path.EndsWith("exe", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var icon = IconExtension.ExtractIconFromExe(path, true);
+                        if (icon == null)
+                        {
+                            return null;
+                        }
+
+                        fileName = Path.ChangeExtension(fileName, ".png");
+                        fileId = Path.ChangeExtension(fileId, ".png");
+                        database.AddFile(fileId, fileName, icon.ToByteArray(System.Drawing.Imaging.ImageFormat.Png));
+                    }
+                    else
+                    {
+                        database.AddFile(fileId, fileName, File.ReadAllBytes(path));
+                    }
+
+                    return fileId;
+                }
+                else
+                {
+                    logger.Error($"Can't add game file during game import, file doesn't exists: {path}");
+                }
+            }
+            catch (Exception e) when (!PlayniteEnvironment.ThrowAllErrors)
+            {
+                logger.Error(e, $"Failed to import game file during game import from {path}");
+            }
+
+            return null;
+        }
+
         public static IEnumerable<Game> ImportGames(ILibraryPlugin library, GameDatabase database)
         {
             foreach (var newGame in library.GetGames())
@@ -21,7 +70,25 @@ namespace Playnite.Database
                 var existingGame = database.GamesCollection.FindOne(a => a.GameId == newGame.GameId && a.PluginId == library.Id);
                 if (existingGame == null)
                 {
-                    logger.Info(string.Format("Adding new game {0} from {1} provider", newGame.GameId, library.Name));
+                    logger.Info(string.Format("Adding new game {0} from {1} plugin", newGame.GameId, library.Name));
+                    if (!string.IsNullOrEmpty(newGame.Icon))
+                    {
+                        newGame.Icon = AddNewFile(newGame.Icon, newGame.Id, database);
+                    }
+
+                    if (!string.IsNullOrEmpty(newGame.CoverImage))
+                    {
+                        newGame.CoverImage = AddNewFile(newGame.CoverImage, newGame.Id, database);
+                    }
+
+                    if (!string.IsNullOrEmpty(newGame.BackgroundImage))
+                    {
+                        if (!newGame.BackgroundImage.StartsWith("http", StringComparison.OrdinalIgnoreCase))
+                        {
+                            newGame.BackgroundImage = AddNewFile(newGame.BackgroundImage, newGame.Id, database);
+                        }
+                    }
+
                     database.AssignPcPlatform(newGame);
                     database.AddGame(newGame);
                     yield return newGame;
