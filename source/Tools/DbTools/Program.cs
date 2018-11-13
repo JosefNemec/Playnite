f23cdf3872cc3fc81afc050300ef0f1ec3322e58
Josef Nemec <nemecjosef@gmail.com>
2018-11-13 11:53:53 +0100

    Disabled dbtools

--- source/Tools/DbTools/Program.cs
+++ source/Tools/DbTools/Program.cs
@@ -15,57 +15,57 @@ namespace DbTools
     {
         static void Main(string[] args)
         {
-            if (args.Count() == 2)
-            {
-                var sourceDb = args[0];
-                var targetDb = args[1];
+            //if (args.Count() == 2)
+            //{
+            //    var sourceDb = args[0];
+            //    var targetDb = args[1];
 
-                if (!File.Exists(sourceDb))
-                {
-                    Console.WriteLine("Source database file not found!");
-                    return;
-                }
+            //    if (!File.Exists(sourceDb))
+            //    {
+            //        Console.WriteLine("Source database file not found!");
+            //        return;
+            //    }
 
-                try
-                {
-                    GameDatabase.CloneLibrary(sourceDb, targetDb);
-                }
-                catch (Exception e) when (!Debugger.IsAttached)
-                {
-                    Console.WriteLine($"Failed to clone database: {e.Message}");
-                }
-            }
-            else
-            {
-                try
-                {
-                    var settings = PlayniteSettings.LoadSettings();
-                    var dbPath = settings.DatabasePath;
-                    var dbTempPath = dbPath + "temp";
+            //    try
+            //    {
+            //        GameDatabase.CloneLibrary(sourceDb, targetDb);
+            //    }
+            //    catch (Exception e) when (!Debugger.IsAttached)
+            //    {
+            //        Console.WriteLine($"Failed to clone database: {e.Message}");
+            //    }
+            //}
+            //else
+            //{
+            //    try
+            //    {
+            //        var settings = PlayniteSettings.LoadSettings();
+            //        var dbPath = settings.DatabasePath;
+            //        var dbTempPath = dbPath + "temp";
 
-                    if (File.Exists(dbTempPath))
-                    {
-                        File.Delete(dbTempPath);
-                    }
+            //        if (File.Exists(dbTempPath))
+            //        {
+            //            File.Delete(dbTempPath);
+            //        }
 
-                    if (!File.Exists(dbPath))
-                    {
-                        Console.WriteLine($"Couldn't find {dbPath} database file.");
-                        return;
-                    }
+            //        if (!File.Exists(dbPath))
+            //        {
+            //            Console.WriteLine($"Couldn't find {dbPath} database file.");
+            //            return;
+            //        }
 
-                    File.Move(dbPath, dbTempPath);
+            //        File.Move(dbPath, dbTempPath);
  
-                    Console.WriteLine($"Fixing {dbPath} file...");
-                    GameDatabase.CloneLibrary(dbTempPath, dbPath);
-                    File.Delete(dbTempPath);
-                    Console.WriteLine("Finished fixing database");
-                }
-                catch (Exception e) when (!Debugger.IsAttached)
-                {
-                    Console.WriteLine($"Failed to clone database: {e.Message}");
-                }
-            }
+            //        Console.WriteLine($"Fixing {dbPath} file...");
+            //        GameDatabase.CloneLibrary(dbTempPath, dbPath);
+            //        File.Delete(dbTempPath);
+            //        Console.WriteLine("Finished fixing database");
+            //    }
+            //    catch (Exception e) when (!Debugger.IsAttached)
+            //    {
+            //        Console.WriteLine($"Failed to clone database: {e.Message}");
+            //    }
+            //}
 
             Console.WriteLine("Press ENTER to exit...");
             Console.ReadLine();
