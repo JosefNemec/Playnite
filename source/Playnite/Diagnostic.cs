2ca46ec7a1cd3943e74d9918c9ef1a326e1ccdb4
Josef Nemec <nemecjosef@gmail.com>
2018-11-19 11:27:56 +0100

    Gather extension config in diag. package

--- source/Playnite/Diagnostic.cs
+++ source/Playnite/Diagnostic.cs
@@ -44,6 +44,16 @@ namespace Playnite
                         archive.CreateEntryFromFile(PlaynitePaths.ConfigFilePath, Path.GetFileName(PlaynitePaths.ConfigFilePath));
                     }
 
+                    // Extension configs
+                    if (Directory.Exists(PlaynitePaths.ExtensionsDataPath))
+                    {
+                        foreach (var cfg in Directory.GetFiles(PlaynitePaths.ExtensionsDataPath, "config.json", SearchOption.AllDirectories))
+                        {
+                            var fileInfo = new FileInfo(cfg);
+                            archive.CreateEntryFromFile(cfg, Path.Combine("extensions", fileInfo.Directory.Name, fileInfo.Name));
+                        }
+                    }
+
                     // System Info
                     var infoPath = Path.Combine(diagTemp, "sysinfo.txt");
                     File.WriteAllText(infoPath, Serialization.ToYaml(Computer.GetSystemInfo()));
