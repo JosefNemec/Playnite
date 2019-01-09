a3fdd5facb48f4189c52b386602c6665bb296457
Josef Nemec <nemecjosef@gmail.com>
2019-01-09 13:55:15 +0100

    Release 4.65

--- source/PlayniteUI/Properties/AssemblyInfo.cs
+++ source/PlayniteUI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Josef Nemec")]
 [assembly: AssemblyProduct("PlayniteUI")]
-[assembly: AssemblyCopyright("Copyright © Josef Nemec 2018")]
+[assembly: AssemblyCopyright("Copyright © Josef Nemec 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,4 +51,4 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.64.0.*")]
+[assembly: AssemblyVersion("4.65.0.*")]
