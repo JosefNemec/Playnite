9dd7435a1abd736c006fe53c3fedb247fd8aa83a
Tyler Szabo <tyler.szabo@gmail.com>
2019-04-05 08:53:09 -0700

    Detect Visual Studio 2017 edition with vswhere (#1037)

--- build/build.ps1
+++ build/build.ps1
@@ -38,6 +38,8 @@
     [string]$TempDir = (Join-Path $env:TEMP "PlayniteBuild")
 )
 
+$VSWHERE_CMD = "vswhere"
+
 $ErrorActionPreference = "Stop"
 & .\common.ps1
 
@@ -150,7 +152,17 @@ if (!$SkipBuild)
     $nugetProc.WaitForExit()
 
     $solutionDir = Join-Path $pwd "..\source"
-    $msbuildPath = "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe";
+
+    if (-not (Get-Command -Name $VSWHERE_CMD -Type Application -ErrorAction Ignore))
+    {
+        $VSWHERE_CMD = (Join-Path $solutionDir "packages\vswhere.2.6.7\tools\vswhere.exe")
+        if (-not (Get-Command -Name $VSWHERE_CMD -Type Application -ErrorAction Ignore))
+        {
+            StartAndWait "nuget.exe" "install vswhere -Version 2.6.7 -SolutionDirectory `"$solutionDir`""
+        }
+    }
+
+    $msbuildpath = & $VSWHERE_CMD -version "[15.0,16.0)" -requires Microsoft.Component.MSBuild -find "MSBuild\**\Bin\MSBuild.exe"
     $arguments = "build.xml /p:SolutionDir=`"$solutionDir`" /p:OutputPath=`"$OutputDir`";Configuration=$configuration /property:Platform=$Platform /t:Build";
     $compilerResult = StartAndWait $msbuildPath $arguments
     if ($compilerResult -ne 0)
