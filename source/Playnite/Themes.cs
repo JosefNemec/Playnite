24e7b3b639e183659ed1b6ed1c4292030d7befa2
Josef Nemec <nemecjosef@gmail.com>
2019-07-29 13:52:35 +0200

    Theme API change

--- source/Playnite/Themes.cs
+++ source/Playnite/Themes.cs
@@ -40,7 +40,7 @@ namespace Playnite
         private static ILogger logger = LogManager.GetLogger();
         public const string ThemeManifestFileName = "theme.yaml";
         public const string PackedThemeFileExtention = ".pthm";
-        public static System.Version ThemeApiVersion => new System.Version("1.0.0");
+        public static System.Version ThemeApiVersion => new System.Version("1.1.0");
         public static ThemeDescription CurrentTheme { get; private set; }
         public static ThemeDescription DefaultTheme { get; private set; } 
 
