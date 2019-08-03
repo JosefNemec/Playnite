9562c1c806b3cb503d578509232fcdb0ecba87fd
Josef Nemec <nemecjosef@gmail.com>
2019-08-03 20:11:57 +0200

    XML comparison tool

--- source/Playnite.Common/Xml.cs
+++ source/Playnite.Common/Xml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,71 @@ namespace Playnite.Common
     public class Xml
     {
 
-        public static bool AreEqual(string xml1, string xml2)
+        public static bool AreEqual(XElement elem1, XElement elem2)
         {
-            return false;
+            if (elem1.Name.ToString() != elem2.Name.ToString())
+            {
+                return false;
+            }
+
+            if (elem1.Value != elem2.Value)
+            {
+                return false;
+            }
+
+            var atts1 = elem1.Attributes().OrderBy(a => a.Name.ToString()).ToList();
+            var atts2 = elem2.Attributes().OrderBy(a => a.Name.ToString()).ToList();
+
+            if (atts1.Count != atts2.Count)
+            {
+                return false;
+            }
+
+            if (atts1.Count > 0)
+            {
+                for (int i = 0; i < atts1.Count; i++)
+                {
+                    var att1 = atts1[i];
+                    var att2 = atts2[i];
+                    if (att1.Name.ToString() != att2.Name.ToString())
+                    {
+                        return false;
+                    }
+
+                    if (att1.Value != att2.Value)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            var elems1 = elem1.Elements().ToList();
+            var elems2 = elem2.Elements().ToList();
+
+            if (elems1.Count != elems2.Count)
+            {
+                return false;
+            }
+
+            if (elems1.Count > 0)
+            {
+                for (int i = 0; i < elems1.Count; i++)
+                {
+                    if (!AreEqual(elems1[i], elems2[i]))
+                    {
+                        return false;
+                    }                        
+                }
+            }
+
+            return true;
         }
 
-        public static bool AreFilesEqual(string xml1, string xml2)
+        public static bool AreEqual(string xml1, string xml2)
         {
-            return false;
+            var xdoc1 = XDocument.Parse(xml1);
+            var xdoc2 = XDocument.Parse(xml2);
+            return AreEqual(xdoc1.Root, xdoc2.Root);
         }
     }
 }
--- source/Tests/Playnite.Tests/XmlTests.cs
+++ source/Tests/Playnite.Tests/XmlTests.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using NUnit.Framework;
 using Playnite;
 using Playnite.Common;
+using System.IO;
 
 namespace Playnite.Tests
 {
@@ -12,10 +13,10 @@ namespace Playnite.Tests
     public class XmlTets
     {
         [Test]
-        public  static void AreEqualStringTest()
+        public static void SimpleAreEqualStringTest()
         {
             Assert.IsTrue(Xml.AreEqual(
-                @"<t><a att='aa' ett='bb'>11</a></t>",
+                @"<t><a att='aa'  ett='bb'>11</a></t>",
                 @"<t><a att='aa' ett='bb'>11</a></t>"));
             Assert.IsTrue(Xml.AreEqual(
                 @"<t><a att='aa' ett='bb'>11</a></t>",
@@ -23,6 +24,29 @@ namespace Playnite.Tests
             Assert.IsTrue(Xml.AreEqual(
                 @"<t><a att='aa'    ett='bb'>11</a>  </t>",
                 @"<t>  <a ett='bb' att='aa'>11</a></t>"));
+
+            Assert.IsFalse(Xml.AreEqual(
+                @"<t><a att='aa' ett='bb'>11</a></t>",
+                @"<t><a att='aa' ett='bb'>22</a></t>"));
+            Assert.IsFalse(Xml.AreEqual(
+                @"<t><a att='aa' ett='aa'>11</a></t>",
+                @"<t><a ett='bb' att='aa'>11</a></t>"));
+            Assert.IsFalse(Xml.AreEqual(
+                @"<t><a att='aa'    ett='bb'>11</a>  </t>",
+                @"<t>  <b ett='bb' att='aa'>11</b></t>"));            
+        }
+
+        [Test]
+        public static void XamlsAreEqualStringTest()
+        {
+            var xml1 = File.ReadAllText(Path.Combine(PlayniteTests.ResourcesPath, "XmlTest", "Xml1.xaml"));
+            var xml2 = File.ReadAllText(Path.Combine(PlayniteTests.ResourcesPath, "XmlTest", "Xml2.xaml"));
+            var xml3 = File.ReadAllText(Path.Combine(PlayniteTests.ResourcesPath, "XmlTest", "Xml3.xaml"));
+            var xml4 = File.ReadAllText(Path.Combine(PlayniteTests.ResourcesPath, "XmlTest", "Xml4.xaml"));
+
+            Assert.IsTrue(Xml.AreEqual(xml1, xml2));
+            Assert.IsFalse(Xml.AreEqual(xml1, xml3));
+            Assert.IsFalse(Xml.AreEqual(xml1, xml4));
         }
     }
 }
