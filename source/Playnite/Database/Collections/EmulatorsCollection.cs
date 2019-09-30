8fbc586e4efa8a92adf6a722052f8de82fe1c0de
Josef Nemec <nemecjosef@gmail.com>
2019-09-30 12:35:46 +0200

    Fix: Remove platform/emulator references when deleting them #513

--- source/Playnite/Database/Collections/EmulatorsCollection.cs
+++ source/Playnite/Database/Collections/EmulatorsCollection.cs
@@ -17,6 +17,43 @@ namespace Playnite.Database
             db = database;
         }
 
+        private void RemoveUsage(Guid id)
+        {
+            foreach (var game in db.Games)
+            {
+                if (game.PlayAction?.Type == GameActionType.Emulator && game.PlayAction?.EmulatorId == id)
+                {
+                    game.PlayAction.EmulatorId = Guid.Empty;
+                    game.PlayAction.EmulatorProfileId = Guid.Empty;
+                    db.Games.Update(game);
+                }
+            }
+        }
+
+        public override bool Remove(Emulator itemToRemove)
+        {
+            RemoveUsage(itemToRemove.Id);
+            return base.Remove(itemToRemove);
+        }
+
+        public override bool Remove(Guid id)
+        {
+            RemoveUsage(id);
+            return base.Remove(id);
+        }
+
+        public override bool Remove(IEnumerable<Emulator> itemsToRemove)
+        {
+            if (itemsToRemove.HasItems())
+            {
+                foreach (var item in itemsToRemove)
+                {
+                    RemoveUsage(item.Id);
+                }
+            }
+            return base.Remove(itemsToRemove);
+        }
+
         public override Emulator Add(string itemName)
         {
             throw new NotSupportedException();
