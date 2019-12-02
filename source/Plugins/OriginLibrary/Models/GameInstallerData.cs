3d4b68d49e6ae7eef4c17031ff67d5f84ea9c97c
Josef Nemec <nemecjosef@gmail.com>
2019-12-02 14:33:51 +0100

    Fix: The Sims 4 Digital Deluxe being imported with wrong settings #1571

---
+++ source/Plugins/OriginLibrary/Models/GameInstallerData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace OriginLibrary.Models
+{
+    [XmlRoot("DiPManifest")]
+    public class GameInstallerData
+    {
+        public class Launcher
+        {
+            public string filePath { get; set; }
+            public string parameters { get; set; }
+            public bool executeElevated { get; set; }
+            public bool requires64BitOS { get; set; }
+            public bool trial { get; set; }
+        }
+
+        public class Runtime
+        {
+            [XmlElement("launcher")]
+            public List<Launcher> launchers { get; set; }
+        }
+
+        [XmlElement("runtime")]
+        public Runtime runtime { get; set; }
+    }
+}
