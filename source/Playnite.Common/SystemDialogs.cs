6da5c4d981e5853b9ec592350533fc24cd066feb
Josef Nemec <nemecjosef@gmail.com>
2020-01-28 15:34:02 +0100

    Small image file selection dialog tweak

--- source/Playnite.Common/SystemDialogs.cs
+++ source/Playnite.Common/SystemDialogs.cs
@@ -120,7 +120,7 @@ namespace Playnite.Common
 
         public static string SelectIconFile(Window owner)
         {
-            return SelectFile(owner, "*.bmp, *.jpg, *.jpeg, *.png, *.gif, *.ico, *.tga, *.exe|*.bmp;*.jpg*;*.png;*.gif;*.ico;*.tga;*.exe");
+            return SelectFile(owner, "Icon Files|*.bmp;*.jpg*;*.png;*.gif;*.ico;*.tga;*.exe;*.tif");
         }
 
         public static string SelectIconFile()
@@ -130,7 +130,7 @@ namespace Playnite.Common
 
         public static string SelectImageFile(Window owner)
         {
-            return SelectFile(owner, "*.bmp, *.jpg, *.jpeg, *.png, *.gif, *.tga|*.bmp;*.jpg*;*.png;*.gif;*.tga");
+            return SelectFile(owner, "Image Files|*.bmp;*.jpg*;*.png;*.gif;*.tga;*.tif");
         }
 
         public static string SelectImageFile()
