d68bec71a3d2e5c7bb4effc6f9dc1c607811a21e
Josef Nemec <nemecjosef@gmail.com>
2020-02-06 16:14:19 +0100

    Humble library icon not being shown

--- source/Plugins/HumbleLibrary/HumbleLibrary.cs
+++ source/Plugins/HumbleLibrary/HumbleLibrary.cs
@@ -5,7 +5,9 @@ using Playnite.SDK.Models;
 using Playnite.SDK.Plugins;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -29,6 +31,10 @@ namespace HumbleLibrary
             CanShutdownClient = false,
             HasCustomizedGameImport = true
         };
+
+        public static string Icon { get; } = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"icon.png");
+
+        public override string LibraryIcon => Icon;
 
         public HumbleLibrary(IPlayniteAPI api) : base(api)
         {
