5562439144aad3a6e2a11a014d6b70233181abd1
Josef Nemec <nemecjosef@gmail.com>
2020-02-06 19:00:25 +0100

    SDK version update

--- source/PlayniteSDK/Properties/AssemblyInfo.cs
+++ source/PlayniteSDK/Properties/AssemblyInfo.cs
@@ -33,10 +33,9 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.1.0.0")]
-[assembly: AssemblyFileVersion("5.1.0.0")]
+[assembly: AssemblyVersion("5.2.0.0")]
+[assembly: AssemblyFileVersion("5.2.0.0")]
 [assembly: InternalsVisibleTo("Playnite.DesktopApp")]
 [assembly: InternalsVisibleTo("Playnite.FullscreenApp")]
 [assembly: InternalsVisibleTo("Playnite.Tests")]
-
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "Playnite.SDK.Models")]
