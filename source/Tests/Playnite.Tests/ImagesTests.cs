96c566fb62553a0ee952a05a0009428d4c6168f7
Josef Nemec <nemecjosef@gmail.com>
2020-03-09 11:17:37 +0100

    Pixel size of .ico files is not reported properly

--- source/Tests/Playnite.Tests/ImagesTests.cs
+++ source/Tests/Playnite.Tests/ImagesTests.cs
@@ -20,5 +20,17 @@ namespace Playnite.Tests
             Assert.AreEqual(256, properties.Height);
             Assert.AreEqual(261, properties.Width);
         }
+
+        [Test]
+        public void GetImagePropertiesIcoTest()
+        {
+            var gw2icon = Images.GetImageProperties(Path.Combine(PlaynitePaths.ProgramPath, "Resources", "Images", "gw2_icon.ico"));
+            Assert.AreEqual(256, gw2icon.Height);
+            Assert.AreEqual(256, gw2icon.Width);
+
+            var mecicon = Images.GetImageProperties(Path.Combine(PlaynitePaths.ProgramPath, "Resources", "Images", "mec_icon.ico"));
+            Assert.AreEqual(256, mecicon.Height);
+            Assert.AreEqual(256, mecicon.Width);
+        }
     }
 }
