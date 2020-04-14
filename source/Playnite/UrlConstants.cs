e4d3071e55c2ac5bab2fff0ffa77cc40f6c82deb
Josef Nemec <nemecjosef@gmail.com>
2020-04-14 14:07:35 +0200

    Updated Discord link

--- source/Playnite/UrlConstants.cs
+++ source/Playnite/UrlConstants.cs
@@ -11,7 +11,7 @@ namespace Playnite
         public const string IssuesTesting = @"https://github.com/JosefNemec/Playnite.Testing/issues/new";
         public const string Issues = @"https://github.com/JosefNemec/Playnite/issues/new/choose";
         public const string Patreon = @"https://www.patreon.com/playnite";
-        public const string Discord = @"https://discord.gg/hSFvmN6";
+        public const string Discord = @"https://discord.gg/BrtABqe";
         public const string Twitter = @"https://twitter.com/AppPlaynite";
         public const string Reddit = @"https://www.reddit.com/r/playnite/";
         public const string SdkDocs = @"https://playnite.link/docs/";
