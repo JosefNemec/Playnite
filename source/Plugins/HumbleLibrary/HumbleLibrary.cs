63e7a5200003228b8a491fd72a220975bf559b7d
Andrew Booze <42682253+awbooze@users.noreply.github.com>
2020-05-07 14:52:52 -0500

    Fix two of three issues (Framed Collection and Into the Breach)

--- source/Plugins/HumbleLibrary/HumbleLibrary.cs
+++ source/Plugins/HumbleLibrary/HumbleLibrary.cs
@@ -132,7 +132,10 @@ namespace HumbleLibrary
                                 {
                                     var exst = allTpks.FirstOrDefault(a =>
                                     !a.human_name.IsNullOrEmpty() &&
-                                    (a.human_name == product.human_name || Regex.IsMatch(a.human_name, product.human_name + @".+\sKey$")));
+                                    (a.human_name.Equals(product.human_name, StringComparison.OrdinalIgnoreCase) || 
+                                    Regex.IsMatch(a.human_name, product.human_name + @".+\sKey$", RegexOptions.IgnoreCase) || 
+                                    Regex.IsMatch(a.human_name, product.human_name + @".*\s\(?Steam\)?$", RegexOptions.IgnoreCase)));
+
                                     if (exst != null)
                                     {
                                         continue;
