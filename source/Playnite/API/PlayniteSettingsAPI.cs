028a46fc6cde72fb334a2557e24581f41ecd7a42
Josef Nemec <nemecjosef@gmail.com>
2020-06-02 13:41:54 +0200

    New: Playnite settings accessible via SDK #1837

--- source/Playnite/API/PlayniteSettingsAPI.cs
+++ source/Playnite/API/PlayniteSettingsAPI.cs
@@ -11,6 +11,28 @@ namespace Playnite.API
     {
         private readonly PlayniteSettings settings;
 
+        public int Version => settings.Version;
+        public int GridItemWidthRatio => settings.GridItemWidthRatio;
+        public int GridItemHeightRatio => settings.GridItemHeightRatio;
+        public bool FirstTimeWizardComplete => settings.FirstTimeWizardComplete;
+        public bool DisableHwAcceleration => settings.DisableHwAcceleration;
+        public bool AsyncImageLoading => settings.AsyncImageLoading;
+        public bool DownloadMetadataOnImport => settings.DownloadMetadataOnImport;
+        public bool StartInFullscreen => settings.StartInFullscreen;
+        public string DatabasePath => settings.DatabasePath;
+        public bool MinimizeToTray => settings.MinimizeToTray;
+        public bool CloseToTray => settings.CloseToTray;
+        public bool EnableTray => settings.EnableTray;
+        public string Language => settings.Language;
+        public bool UpdateLibStartup => settings.UpdateLibStartup;
+        public string DesktopTheme => settings.Theme;
+        public string FullscreenTheme => settings.Fullscreen.Theme;
+        public bool StartMinimized => settings.StartMinimized;
+        public bool StartOnBoot => settings.StartOnBoot;
+        public bool ForcePlayTimeSync => settings.ForcePlayTimeSync;
+        public string FontFamilyName => settings.FontFamilyName;
+        public bool DiscordPresenceEnabled => settings.DiscordPresenceEnabled;
+
         public PlayniteSettingsAPI(PlayniteSettings settings)
         {
             this.settings = settings;
--- source/PlayniteSDK/IPlayniteSettingsAPI.cs
+++ source/PlayniteSDK/IPlayniteSettingsAPI.cs
@@ -12,6 +12,111 @@ namespace Playnite.SDK
     public interface IPlayniteSettingsAPI
     {
         /// <summary>
+        ///
+        /// </summary>
+        int Version { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        int GridItemWidthRatio { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        int GridItemHeightRatio { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool FirstTimeWizardComplete { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool DisableHwAcceleration { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool AsyncImageLoading { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool DownloadMetadataOnImport { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool StartInFullscreen { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        string DatabasePath { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool MinimizeToTray { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool CloseToTray { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool EnableTray { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        string Language { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool UpdateLibStartup { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        string DesktopTheme { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        string FullscreenTheme { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool StartMinimized { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool StartOnBoot { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool ForcePlayTimeSync { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        string FontFamilyName { get; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        bool DiscordPresenceEnabled { get; }
+
+        /// <summary>
         /// Checks if game is added on import exclusion list.
         /// </summary>
         /// <param name="gameId">Game ID.</param>
