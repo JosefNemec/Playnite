5d2b79c9a6d7d6b31cf7a4ebd13eeb36525b50a9
Josef Nemec <nemecjosef@gmail.com>
2020-06-16 16:17:40 +0200

    Fix: Battle.net authentication is invalidated very often #1057

--- source/Plugins/BattleNetLibrary/Services/BattleNetAccountClient.cs
+++ source/Plugins/BattleNetLibrary/Services/BattleNetAccountClient.cs
@@ -91,6 +91,8 @@ namespace BattleNetLibrary.Services
 
         public BattleNetApiStatus GetApiStatus()
         {
+            // This refreshes authentication cookie
+            webView.NavigateAndWait("https://account.blizzard.com:443/oauth2/authorization/account-settings");
             webView.NavigateAndWait(apiStatusUrl);
             var textStatus = webView.GetPageText();
             return Serialization.FromJson<BattleNetApiStatus>(textStatus);
