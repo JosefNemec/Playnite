84706ab6b21ef542f9f0b0ae1056c5717d300f22
Josef Nemec <nemecjosef@gmail.com>
2020-07-22 17:17:55 +0200

    Adjusted min/max zoom values on Grid view

--- source/Playnite/Settings/ViewProperties.cs
+++ source/Playnite/Settings/ViewProperties.cs
@@ -596,9 +596,9 @@ namespace Playnite
 
     public class ViewSettings : ObservableObject
     {
-        public const double MinGridItemWidth = 120;
+        public const double MinGridItemWidth = 60;
         public const double DefaultGridItemWidth = 200;
-        public const double MaxGridItemWidth = 560;
+        public const double MaxGridItemWidth = 700;
 
         private SortOrder sortingOrder = SortOrder.Name;
         public SortOrder SortingOrder
