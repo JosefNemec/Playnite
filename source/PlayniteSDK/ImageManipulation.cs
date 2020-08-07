55ee5d1caee4441b3df49c89304e0d97cdf339c8
erri120 <erri120@protonmail.com>
2020-08-07 12:32:50 +0200

    Created Resize and Downscale Image Manipulation methods

---
+++ source/PlayniteSDK/ImageManipulation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Playnite.SDK
+{
+    /// <summary>
+    /// Utility functions for Image Manipulation
+    /// </summary>
+    public static class ImageManipulation
+    {
+        /// <summary>
+        /// Resize an image.
+        /// </summary>
+        /// <param name="image">Image to resize</param>
+        /// <param name="maxWidth">Max width the image should have</param>
+        /// <param name="maxHeight">Max height the image should have</param>
+        /// <returns></returns>
+        public static byte[] ResizeImage(Image image, int maxWidth, int maxHeight)
+        {
+            if(image == null)
+                throw new ArgumentNullException(nameof(image));
+
+            var desiredWidth = image.Width > maxWidth ? maxWidth : image.Width;
+            var desiredHeight = image.Height > maxHeight ? maxHeight : image.Height;
+
+            using (var resizedImage = image.GetThumbnailImage(desiredWidth, desiredHeight, null, IntPtr.Zero))
+            using (var stream = new MemoryStream())
+            {
+                resizedImage.Save(stream, ImageFormat.Png);
+                stream.Position = 0;
+                return stream.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Downscale an Image percentage based
+        /// </summary>
+        /// <param name="image">Image to downscale</param>
+        /// <param name="factor">Downscale percentage, must be between 0 and 1</param>
+        /// <returns></returns>
+        public static byte[] DownscaleImage(Image image, double factor = 0.5)
+        {
+            if (image == null)
+                throw new ArgumentNullException(nameof(image));
+
+            if(factor >= 1 || factor <= double.Epsilon)
+                throw new ArgumentException($"Factor can only be between 0 and 1! {factor}", nameof(factor));
+
+            return ResizeImage(image, (int)(image.Width * factor), (int)(image.Height * factor));
+        }
+    }
+}
---
+++ source/Tests/Playnite.Tests/ImageManipulationTests.cs
@@ -0,0 +1,63 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using Playnite.Common;
+using Playnite.SDK;
+
+namespace Playnite.Tests
+{
+    [TestFixture]
+    public class ImageManipulationTests
+    {
+        [Test]
+        public void ResizeImageTest()
+        {
+            var baseDir = Assembly.GetExecutingAssembly().Location;
+            Assert.NotNull(baseDir);
+            baseDir = Path.GetDirectoryName(baseDir);
+            var path = Path.Combine(baseDir, "Resources", "Images", "DesignCover.jpg");
+            Assert.True(File.Exists(path));
+
+            var output = Path.Combine(baseDir, "image-manipulation-resize-output.png");
+
+            using (var image = Image.FromFile(path))
+            {
+                Assert.AreEqual(262, image.Width);
+                Assert.AreEqual(374, image.Height);
+
+                byte[] result = ImageManipulation.ResizeImage(image, 200, 285);
+                File.WriteAllBytes(output, result);
+            }
+
+            var outputProperties = Images.GetImageProperties(output);
+            Assert.AreEqual(200, outputProperties.Width);
+            Assert.AreEqual(285, outputProperties.Height);
+        }
+
+        [Test]
+        public void DownscaleTest()
+        {
+            var baseDir = Assembly.GetExecutingAssembly().Location;
+            Assert.NotNull(baseDir);
+            baseDir = Path.GetDirectoryName(baseDir);
+            var path = Path.Combine(baseDir, "Resources", "Images", "DesignCover.jpg");
+            Assert.True(File.Exists(path));
+
+            var output = Path.Combine(baseDir, "image-manipulation-downscale-output.png");
+
+            using (var image = Image.FromFile(path))
+            {
+                Assert.AreEqual(262, image.Width);
+                Assert.AreEqual(374, image.Height);
+
+                byte[] result = ImageManipulation.DownscaleImage(image, 0.25);
+                File.WriteAllBytes(output, result);
+            }
+
+            var outputProperties = Images.GetImageProperties(output);
+            Assert.AreEqual(65, outputProperties.Width);
+            Assert.AreEqual(93, outputProperties.Height);
+        }
+    }
+}
