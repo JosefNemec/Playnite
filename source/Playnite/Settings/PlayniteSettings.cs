a6d47c2f9f975505031e93ee744a8baa8b9a537f
Josef Nemec <nemecjosef@gmail.com>
2020-08-12 11:18:01 +0200

    Change date format in logs

--- source/Playnite/Settings/PlayniteSettings.cs
+++ source/Playnite/Settings/PlayniteSettings.cs
@@ -1732,7 +1732,7 @@ namespace Playnite
             var fileTarget = new FileTarget()
             {
                 FileName = Path.Combine(PlaynitePaths.ConfigRootPath, "playnite.log"),
-                Layout = "${longdate}|${level:uppercase=true}|${logger}:${message}${exception:format=toString}",
+                Layout = "${date:format=dd-MM HH\\:mm\\:ss.fff}|${level:uppercase=true}|${logger}:${message}${exception:format=toString}",
                 KeepFileOpen = false,
                 ArchiveFileName = Path.Combine(PlaynitePaths.ConfigRootPath, "playnite.{#####}.log"),
                 ArchiveAboveSize = 4096000,
