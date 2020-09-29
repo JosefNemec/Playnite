a645b226761a31406a1facb558d699b266c27835
Josef Nemec <nemecjosef@gmail.com>
2020-09-29 12:39:14 +0200

    Playnite 8 theme changelog

--- source/Playnite/Themes.cs
+++ source/Playnite/Themes.cs
@@ -72,8 +72,8 @@ namespace Playnite
     public class ThemeManager
     {
         private static ILogger logger = LogManager.GetLogger();
-        public static System.Version DesktopApiVersion => new System.Version("1.6.0");
-        public static System.Version FullscreenApiVersion => new System.Version("1.6.0");
+        public static System.Version DesktopApiVersion => new System.Version("1.7.0");
+        public static System.Version FullscreenApiVersion => new System.Version("1.7.0");
         public static ThemeManifest CurrentTheme { get; private set; }
         public static ThemeManifest DefaultTheme { get; private set; }
 
