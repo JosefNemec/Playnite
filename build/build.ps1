b32d425d442f95f5e4b67ddd5741bc81a129c759
Josef Nemec <nemecjosef@gmail.com>
2020-10-06 17:52:15 +0200

    Removed PlayniteUI.exe

--- build/build.ps1
+++ build/build.ps1
@@ -183,7 +183,6 @@ if (!$SkipBuild)
             Join-Path $OutputDir "Playnite.SDK.dll" | SignFile
             Join-Path $OutputDir "Playnite.DesktopApp.exe" | SignFile
             Join-Path $OutputDir "Playnite.FullscreenApp.exe" | SignFile
-            Join-Path $OutputDir "PlayniteUI.exe" | SignFile
         }
     }
 
