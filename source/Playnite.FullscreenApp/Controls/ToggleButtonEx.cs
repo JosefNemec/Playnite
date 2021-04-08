6929d3d8086bc616c1fc0cef7995044f73b6bf2c
Josef Nemec <nemecjosef@gmail.com>
2021-04-08 08:24:41 +0200

    ToggleButtonEx for Fullscreen mode #2292

---
+++ source/Playnite.FullscreenApp/Controls/ToggleButtonEx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using Playnite.Input;
+
+namespace Playnite.FullscreenApp.Controls
+{
+    public class ToggleButtonEx : ToggleButton
+    {
+        static ToggleButtonEx()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ToggleButtonEx), new FrameworkPropertyMetadata(typeof(ToggleButtonEx)));
+        }
+
+        public ToggleButtonEx() : base()
+        {
+            KeyDown += Ex_KeyDown;
+        }
+
+        private void Ex_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e is XInputEventArgs xinput)
+            {
+                if (xinput.XButton == XInputButton.A)
+                {
+                    OnClick();
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
