b573de4095dca282afe9f0ef5e48aa60d7c49dd1
Josef Nemec <nemecjosef@gmail.com>
2021-04-14 15:31:33 +0200

    Disabled library integration settings in main settings window

--- source/Playnite.DesktopApp/ViewModels/SettingsViewModel.cs
+++ source/Playnite.DesktopApp/ViewModels/SettingsViewModel.cs
@@ -196,7 +196,7 @@ namespace Playnite.DesktopApp.ViewModels
                 { 7, new Controls.SettingsSections.Input() { DataContext = this } },
                 { 8, new Controls.SettingsSections.AddonsMoveInfo() { DataContext = this } },
                 { 9, new Controls.SettingsSections.Metadata() { DataContext = this } },
-                { 10, new Controls.SettingsSections.EmptyParent() { DataContext = this } },
+                { 10, new Controls.SettingsSections.IntegrationsMoveInfo() { DataContext = this } },
                 { 11, new Controls.SettingsSections.Scripting() { DataContext = this } },
                 { 12, new Controls.SettingsSections.ClientShutdown() { DataContext = this } },
                 { 13, new Controls.SettingsSections.Performance() { DataContext = this } },
