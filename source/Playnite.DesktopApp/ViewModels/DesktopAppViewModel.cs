65df5b939712a5dc6145f285fcff04f84824034c
Josef Nemec <nemecjosef@gmail.com>
2021-09-07 13:15:47 +0200

    Check for updates menu item checks for add-on updates as well

--- source/Playnite.DesktopApp/ViewModels/DesktopAppViewModel.cs
+++ source/Playnite.DesktopApp/ViewModels/DesktopAppViewModel.cs
@@ -750,6 +750,10 @@ namespace Playnite.DesktopApp.ViewModels
                 Logger.Error(e, "Failed to check for update.");
                 Dialogs.ShowErrorMessage(Resources.GetString("LOCUpdateCheckFailMessage"), Resources.GetString("LOCUpdateError"));
             }
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            CheckForAddonUpdates();
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 
         public void SwitchToFullscreenMode()
