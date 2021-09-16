72c849f684cd07da95cf38cb96d2ff62792b7b10
Josef Nemec <nemecjosef@gmail.com>
2021-09-16 11:07:39 +0200

    ulong fields on game edit dialog don't behave correctly #2464

--- source/Playnite/Converters/NumericConverters.cs
+++ source/Playnite/Converters/NumericConverters.cs
@@ -10,6 +10,78 @@ using System.Windows.Markup;
 
 namespace Playnite.Converters
 {
+    #region ulong
+
+    public class UlongToStringConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                throw new NotSupportedException();
+            }
+            else if (value is ulong num)
+            {
+                return num.ToString();
+            }
+
+            throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var str = (string)value;
+            if (str.IsNullOrEmpty())
+            {
+                throw new NotSupportedException();
+            }
+            else
+            {
+                return ulong.Parse(str);
+            }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+
+    public class UlongFieldValidation : ValidationRule
+    {
+        private string invalidInput => $"Not an ulong value in {MinValue} to {MaxValue} range!";
+
+        public ulong MinValue { get; set; } = 0;
+        public ulong MaxValue { get; set; } = ulong.MaxValue;
+
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            if (value == null)
+            {
+                return new ValidationResult(true, null);
+            }
+            else
+            {
+                var str = (string)value;
+                if (str.IsNullOrEmpty())
+                {
+                    return new ValidationResult(false, invalidInput);
+                }
+
+                if (ulong.TryParse(str, out var intVal) && intVal >= MinValue && intVal <= MaxValue)
+                {
+                    return new ValidationResult(true, null);
+                }
+
+                return new ValidationResult(false, invalidInput);
+            }
+        }
+    }
+
+    #endregion ulong
+
+    #region null int
+
     public class NullableIntToStringConverter : MarkupExtension, IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
@@ -49,8 +121,8 @@ namespace Playnite.Converters
     {
         private string invalidInput => $"Not an integer value in {MinValue} to {MaxValue} range!";
 
-        public int MinValue { get; set; }
-        public int MaxValue { get; set; }
+        public int MinValue { get; set; } = 0;
+        public int MaxValue { get; set; } = int.MaxValue;
 
         public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
         {
@@ -75,4 +147,6 @@ namespace Playnite.Converters
             }
         }
     }
+
+    #endregion null int
 }
