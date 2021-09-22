48391e74bdd9916555653ebc0adca300256e61c1
Josef Nemec <nemecjosef@gmail.com>
2021-09-22 10:34:08 +0200

    Some fields don't work on List view #2498

--- source/Playnite/Converters/ListToStringConverter.cs
+++ source/Playnite/Converters/ListToStringConverter.cs
@@ -9,6 +9,45 @@ using System.Windows.Markup;
 
 namespace Playnite.Converters
 {
+    public class NiceListToStringConverter : MarkupExtension, IValueConverter
+    {        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return string.Empty;
+            }
+
+            return string.Join(", ", (IEnumerable<object>)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var stringVal = (string)value;
+
+            if (string.IsNullOrEmpty(stringVal))
+            {
+                return null;
+            }
+            else
+            {
+                var converted = stringVal.Split(new char[] { ',' }).Select(a => a.Trim());
+                if (targetType == typeof(ComparableList<string>))
+                {
+                    return new ComparableList<string>(converted);
+                }
+                else
+                {
+                    return converted.ToList();
+                }
+            }
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+
     public class ListToStringConverter : MarkupExtension, IValueConverter
     {
         public static string MakeString(IEnumerable<string> source)
