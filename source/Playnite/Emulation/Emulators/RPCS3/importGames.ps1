5717e9a01cf0fefc3cf88e0e09ca24b444f1df9d
Jeshibu <jeshibu@gmail.com>
2021-10-04 19:28:00 +0200

    Style matching

--- source/Playnite/Emulation/Emulators/RPCS3/importGames.ps1
+++ source/Playnite/Emulation/Emulators/RPCS3/importGames.ps1
@@ -7,19 +7,22 @@ if (-not [System.IO.Directory]::Exists($ImportArgs.ScanDirectory))
     return
 }
 
-function Get-NullTerminatedString{
+function Get-NullTerminatedString
+{
     param([Array]$bytes, [int]$offset)
 
-    $str_bytes = @()
-    for ($j = $offset; $j -lt $bytes.Count; $j++) {
+    $strBytes = @()
+    for ($j = $offset; $j -lt $bytes.Count; $j++)
+    {
         $b = $bytes[$j]
         if($b -eq 0) { break } #strings end on null terminator
-        $str_bytes += $b
+        $strBytes += $b
     }
-    return [System.Text.Encoding]::UTF8.GetString($str_bytes)
+    return [System.Text.Encoding]::UTF8.GetString($strBytes)
 }
 
-function Get-ParamSfoValue {
+function Get-ParamSfoValue
+{
     param([string]$path, [string]$key)
 
     #thanks to https://psdevwiki.com/ps3/PARAM.SFO
@@ -31,19 +34,24 @@ function Get-ParamSfoValue {
     $indexRowLength = 0x10
 
     #go through each index table row
-    for ($i = $indexTableOffset; $i -lt $keyTableOffset; $i += $indexRowLength) {
+    for ($i = $indexTableOffset; $i -lt $keyTableOffset; $i += $indexRowLength)
+    {
         $relativeKeyOffset = [System.BitConverter]::ToUInt16($bytes, $i)
         $foundKey = Get-NullTerminatedString $bytes ($keyTableOffset + $relativeKeyOffset)
-        if($foundKey -ne $key) {
+        if($foundKey -ne $key)
+        {
             continue
         }
 
         $dataFormat = [System.BitConverter]::ToUInt16($bytes, $i + 0x02)
         $dataLength = [System.BitConverter]::ToUInt32($bytes, $i + 0x04)
         $relativeDataOffset = [System.BitConverter]::ToUInt32($bytes, $i + 0x0c)
-        if($dataFormat -eq 1028){ #uint32
+        if ($dataFormat -eq 1028) #uint32
+        {
             $data = [System.BitConverter]::ToUInt32($bytes, $dataTableOffset + $relativeDataOffset)
-        }else{ #string (usually null-terminated)
+        }
+        else #string (usually null-terminated)
+        {
             $data = [System.Text.Encoding]::UTF8.GetString($bytes, $dataTableOffset + $relativeDataOffset, $dataLength).Trim("`0")
         }
         return $data
@@ -66,9 +74,11 @@ foreach ($game in $games)
     $parentDir = $game.Directory.Parent.FullName
     $paramSfoPath = Join-Path $parentDir "PARAM.SFO"
 
-    if (Test-Path $paramSfoPath -PathType Leaf){
+    if (Test-Path $paramSfoPath -PathType Leaf)
+    {
         $scannedGame.Serial = Get-ParamSfoValue $paramSfoPath "TITLE_ID"
-        if ($null -ne $scannedGame.Serial){
+        if ($null -ne $scannedGame.Serial)
+        {
             $scannedGame.Name = Get-ParamSfoValue $paramSfoPath "TITLE"
             $scannedGame
         }
