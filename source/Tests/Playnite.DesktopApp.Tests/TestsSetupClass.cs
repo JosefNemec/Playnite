44319d0d794ad35a5605a24056cf5f9cf4d3ff88
Josef Nemec <nemecjosef@gmail.com>
2021-11-27 13:08:03 +0100

    Some tests failing

--- source/Tests/Playnite.DesktopApp.Tests/TestsSetupClass.cs
+++ source/Tests/Playnite.DesktopApp.Tests/TestsSetupClass.cs
@@ -5,8 +5,10 @@ using Playnite.Tests;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Playnite.DesktopApp.Tests
 {
@@ -16,13 +18,17 @@ namespace Playnite.DesktopApp.Tests
         [OneTimeSetUp]
         public void GlobalSetup()
         {
+            // To register pack:// scheme
+            var current = new Application();
+            PlayniteTests.SetEntryAssembly(Assembly.GetExecutingAssembly());
             FileSystem.CreateDirectory(PlayniteTests.TempPath, true);
+            NLogLogger.IsTraceEnabled = true;
+            PlayniteSettings.ConfigureLogger();
         }
 
         [OneTimeTearDown]
         public void GlobalTeardown()
         {
-
         }
     }
 }
