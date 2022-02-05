09c59a9fec4cef2bebd1436f13e8f6236143b751
erri120 <erri120@protonmail.com>
2022-02-05 15:18:45 +0100

    Convert Playnite.DesktopApp

--- source/Playnite/Properties/AssemblyInfo.cs
+++ source/Playnite/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Markup;
 
-[assembly: System.Windows.ThemeInfo(System.Windows.ResourceDictionaryLocation.None, System.Windows.ResourceDictionaryLocation.SourceAssembly)]
-
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: InternalsVisibleTo("Playnite.Tests")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "Playnite")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "Playnite.Controls")]
