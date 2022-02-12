2cc6dcb18e6a6f6d43f356d83844af1119e10eb0
UrbanCMC <UrbanCMC@web.de>
2022-02-12 14:45:47 +0100

    Restore unused usings

--- source/Playnite.DesktopApp/Controls/NumericDoubleBox.cs
+++ source/Playnite.DesktopApp/Controls/NumericDoubleBox.cs
@@ -1,10 +1,19 @@
 ﻿using Playnite.Common;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace Playnite.DesktopApp.Controls
 {
