31602a43fea30a2fc9c17b48caede8bc76240fa5
Josef Nemec <nemecjosef@gmail.com>
2022-04-20 18:21:23 +0200

    Filter invalid classname characters #2835

---
+++ source/Tests/Playnite.Toolbox.Tests/ExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Playnite.SDK;
+
+namespace Playnite.Toolbox.Tests
+{
+    [TestFixture]
+    public class ExtensionsTests
+    {
+        [Test]
+        public void ConvertToValidIdentifierNameTest()
+        {
+            Assert.AreEqual("itchio2", Extensions.ConvertToValidIdentifierName("itch.io@$#% 2"));
+        }
+    }
+}
