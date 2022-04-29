21f005bfefff5000acd6723a4ecc672f2f3f6a76
V0idhead <x>
2022-04-29 19:47:16 +0200

    apply changes from actionselect branch add adapted handling for number keys

--- source/Playnite.DesktopApp/Windows/ActionSelectionWindow.xaml.cs
+++ source/Playnite.DesktopApp/Windows/ActionSelectionWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using Playnite.Controls;
-using Playnite.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Playnite.Controls;
 
 namespace Playnite.DesktopApp.Windows
 {
@@ -11,6 +15,23 @@ namespace Playnite.DesktopApp.Windows
         public ActionSelectionWindow() : base()
         {
             InitializeComponent();
+        }
+
+        private void WindowActionSelectionWindow_ContentRendered(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 9; ++i)
+            {
+                DependencyObject item = ActionItems.ItemContainerGenerator.ContainerFromIndex(i);
+                if (item == null)
+                {
+                    break; //less than 9 items
+                }
+
+                Button actionButton = (Button)VisualTreeHelper.GetChild(item, 0);
+                Key key = (Key)Enum.Parse(typeof(Key), "D" + (i + 1));
+                KeyBinding newBinding = new KeyBinding() { Command = actionButton.Command, CommandParameter = actionButton.CommandParameter, Key = key };
+                InputBindings.Add(newBinding);
+            }
         }
     }
 }
