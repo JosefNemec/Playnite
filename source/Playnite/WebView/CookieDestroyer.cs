eeef95804c96bd355413bb7f6c6978da83d20275
Josef Nemec <nemecjosef@gmail.com>
2022-07-22 14:38:53 +0200

    Fixed handling of cookie related methods in web views

--- source/Playnite/WebView/CookieDestroyer.cs
+++ source/Playnite/WebView/CookieDestroyer.cs
@@ -1,8 +1,10 @@
 ﻿using CefSharp;
+using Playnite.SDK;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Playnite.WebView
@@ -10,6 +12,8 @@ namespace Playnite.WebView
     public class CookieDestroyer : ICookieVisitor
     {
         private readonly string domainName;
+
+        internal readonly AutoResetEvent Finished = new AutoResetEvent(false);
 
         public CookieDestroyer(string domainName)
         {
@@ -27,7 +31,83 @@ namespace Playnite.WebView
                 deleteCookie = true;
             }
 
+            if (count == total - 1)
+            {
+                Finished.Set();
+            }
+
             return true;
+        }
+    }
+
+    public class StandardCookieVisitor : ICookieVisitor
+    {
+        public readonly List<HttpCookie> Cookies = new List<HttpCookie>();
+        internal readonly AutoResetEvent Finished = new AutoResetEvent(false);
+
+        public StandardCookieVisitor()
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public bool Visit(Cookie cookie, int count, int total, ref bool deleteCookie)
+        {
+            Cookies.Add(new HttpCookie
+            {
+                Name = cookie.Name,
+                Value = cookie.Value,
+                Domain = cookie.Domain,
+                Path = cookie.Path,
+                Expires = cookie.Expires,
+                Creation = cookie.Creation,
+                HttpOnly = cookie.HttpOnly,
+                LastAccess = cookie.LastAccess,
+                Priority = (CookiePriority)(int)cookie.Priority,
+                SameSite = (CookieSameSite)(int)cookie.SameSite,
+                Secure = cookie.Secure
+            });
+
+            if (count == total - 1)
+            {
+                Finished.Set();
+            }
+
+            return true;
+        }
+    }
+
+    public class DeleteCookiesHandler : IDeleteCookiesCallback
+    {
+        internal readonly AutoResetEvent Finished = new AutoResetEvent(false);
+        public bool IsDisposed { get; private set; }
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+        }
+
+        public void OnComplete(int numDeleted)
+        {
+            Finished.Set();
+        }
+    }
+
+    public class SetCookieHandler : ISetCookieCallback
+    {
+        internal readonly AutoResetEvent Finished = new AutoResetEvent(false);
+        public bool IsDisposed { get; private set; }
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+        }
+
+        public void OnComplete(bool success)
+        {
+            Finished.Set();
         }
     }
 }
