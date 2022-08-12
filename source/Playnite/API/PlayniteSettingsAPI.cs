5ba9fdaf8a26038591d49ba7644ed385ee776e47
Brandon <brandon_corchado1@hotmail.com>
2022-08-12 02:33:13 -0500

    Expose InstallSizeScanUseSizeOnDisk field in Settings API

--- source/Playnite/API/PlayniteSettingsAPI.cs
+++ source/Playnite/API/PlayniteSettingsAPI.cs
@@ -50,6 +50,7 @@ namespace Playnite.API
         public bool DisableHwAcceleration => settings.DisableHwAcceleration;
         public bool AsyncImageLoading => settings.AsyncImageLoading;
         public bool DownloadMetadataOnImport => settings.DownloadMetadataOnImport;
+        public bool InstallSizeScanUseSizeOnDisk => settings.InstallSizeScanUseSizeOnDisk;
         public bool StartInFullscreen => settings.StartInFullscreen;
         public string DatabasePath => settings.DatabasePath;
         public bool MinimizeToTray => settings.MinimizeToTray;
