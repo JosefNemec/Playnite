766d355a832a392465d9550b2932c4d527300782
Josef Nemec <nemecjosef@gmail.com>
2022-12-10 10:45:15 +0100

    Removed forum link

--- source/Playnite.DesktopApp/Controls/Menus/MainMenu.cs
+++ source/Playnite.DesktopApp/Controls/Menus/MainMenu.cs
@@ -183,10 +183,9 @@ namespace Playnite.DesktopApp.Controls
 
             // Links
             var linksItem = AddMenuChild(Items, "LOCMenuLinksTitle", null);
-            AddMenuChild(linksItem.Items, "LOCCommonLinksForum", GlobalCommands.NavigateUrlCommand, UrlConstants.Forum, "Images/applogo.png");
             AddMenuChild(linksItem.Items, "Discord", GlobalCommands.NavigateUrlCommand, UrlConstants.Discord, "Images/discord.png");
-            AddMenuChild(linksItem.Items, "Twitter", GlobalCommands.NavigateUrlCommand, UrlConstants.Twitter, "Images/twitter.png");
             AddMenuChild(linksItem.Items, "Reddit", GlobalCommands.NavigateUrlCommand, UrlConstants.Reddit, "Images/reddit.png");
+            AddMenuChild(linksItem.Items, "Twitter", GlobalCommands.NavigateUrlCommand, UrlConstants.Twitter, "Images/twitter.png");
 
             // Help
             var helpItem = AddMenuChild(Items, "LOCMenuHelpTitle", null);
