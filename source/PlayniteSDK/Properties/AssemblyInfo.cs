b49bbd4f3b03686efd8fb23f9f52d36dd543a290
Josef Nemec <nemecjosef@gmail.com>
2022-12-14 11:47:47 +0100

    SDK version bump

--- source/PlayniteSDK/Properties/AssemblyInfo.cs
+++ source/PlayniteSDK/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Windows.Markup;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("6.7.0.0")]
-[assembly: AssemblyFileVersion("6.7.0.0")]
+[assembly: AssemblyVersion("6.8.0.0")]
+[assembly: AssemblyFileVersion("6.8.0.0")]
 [assembly: InternalsVisibleTo("Playnite.DesktopApp")]
 [assembly: InternalsVisibleTo("Playnite.FullscreenApp")]
 [assembly: InternalsVisibleTo("Playnite.Tests")]
