4efc2fb6c4353c74a7d770fb0fdb85123385d34b
Josef Nemec <nemecjosef@gmail.com>
2024-08-12 13:39:36 +0200

    CefSharp updated

--- source/Playnite/CefTools.cs
+++ source/Playnite/CefTools.cs
@@ -34,6 +34,14 @@ namespace Playnite
             settings.CefCommandLineArgs.Add("disable-gpu", "1");
             settings.CefCommandLineArgs.Add("disable-gpu-compositing", "1");
 
+            // This is because cookies created with Alloy runtime won't work with Chrome runtime
+            // which is default since CefSharp 126. Alloy will be completely removed from CEF 128
+            // so P10 will be likely stuck forever on 127.
+            // https://github.com/cefsharp/CefSharp/issues/4847
+            // https://github.com/chromiumembedded/cef/issues/3721
+            settings.ChromeRuntime = false;
+            CefSharpSettings.RuntimeStyle = CefRuntimeStyle.Alloy;
+
             settings.CachePath = PlaynitePaths.BrowserCachePath;
             settings.PersistSessionCookies = true;
             settings.LogFile = Path.Combine(PlaynitePaths.ConfigRootPath, "cef.log");
@@ -41,7 +49,7 @@ namespace Playnite
             // Firefox user agent gives the best compatibility because some websites complain
             // about unsecure browser if we try to pretend to be Chrome (which is CefSharp's default).
             // Plugins can change this on an individual level anyways.
-            settings.UserAgent = $"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0 Playnite/{PlayniteApplication.CurrentVersion.ToString(2)}";
+            settings.UserAgent = $"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0 Playnite/{PlayniteApplication.CurrentVersion.ToString(2)}";
             IsInitialized = Cef.Initialize(settings);
         }
 
