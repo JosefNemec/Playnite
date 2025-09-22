83320df7dfeffbd3c6a5bbf039060f9a75110200
Josef Nemec <nemecjosef@proton.me>
2025-09-22 20:32:50 +0200

    New: Added CTRL-SHIFT-P shortcut to Fullscreen mode for opening SDK PowerShell instance

--- source/Playnite.FullscreenApp/Controls/Views/Main.cs
+++ source/Playnite.FullscreenApp/Controls/Views/Main.cs
@@ -238,6 +238,7 @@ namespace Playnite.FullscreenApp.Controls.Views
                     ViewHost.InputBindings.Add(new KeyBinding() { Command = mainModel.ToggleFiltersCommand, Key = Key.F });
                     ViewHost.InputBindings.Add(new KeyBinding() { Command = mainModel.SwitchToDesktopCommand, Key = Key.F11 });
                     ViewHost.InputBindings.Add(new KeyBinding() { Command = mainModel.SelectFilterPresetCommand, Key = Key.R });
+                    ViewHost.InputBindings.Add(new KeyBinding() { Command = mainModel.StartInteractivePowerShellCommand, Key = Key.P, Modifiers = ModifierKeys.Control | ModifierKeys.Shift });
 
                     ViewHost.InputBindings.Add(new GameControllerInputBinding(mainModel.PrevFilterViewCommand, ControllerInput.LeftShoulder));
                     ViewHost.InputBindings.Add(new GameControllerInputBinding(mainModel.NextFilterViewCommand, ControllerInput.RightShoulder));
