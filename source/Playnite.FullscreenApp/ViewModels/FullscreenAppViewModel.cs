7b70f2712f89f5680c5599b4cba41257038a4ec2
Denis Medvedev <medvedev.denision@gmail.com>
2025-10-28 16:33:13 +0300

    Fix: Window restoration in Fullscreen mode (by DenisionSoft)
    * Fix window restoration in Fullscreen mode
    
    * Revert RestoreWindows fallbacks
    
    * chore

--- source/Playnite.FullscreenApp/ViewModels/FullscreenAppViewModel.cs
+++ source/Playnite.FullscreenApp/ViewModels/FullscreenAppViewModel.cs
@@ -1026,7 +1026,7 @@ namespace Playnite.FullscreenApp.ViewModels
 
         public void RestoreWindow()
         {
-            WindowManager.LastActiveWindow?.RestoreWindow();
+            Window.RestoreWindow();
         }
 
         public void MinimizeWindow()
