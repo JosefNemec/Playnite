062669a73907f01bc85c004a803da74fe7640475
darklinkpower <brandon_corchado1@hotmail.com>
2026-05-15 19:34:51 -0600

    Create SearchMode enum and create Filters settings for Name and Version searches

--- source/PlayniteSDK/Models/FilterPreset.cs
+++ source/PlayniteSDK/Models/FilterPreset.cs
@@ -80,6 +80,13 @@ namespace Playnite.SDK.Models
         [Description("LOCRecentActivityLabel")] RecentActivity = 26
     }
 
+    public enum SearchMode : int
+    {
+        Normal = 0,
+        Fuzzy = 1,
+        Regex = 2
+    }
+
     public class FilterPresetSettings
     {
         public bool UseAndFilteringStyle { get; set; }
@@ -88,7 +95,9 @@ namespace Playnite.SDK.Models
         public bool Hidden { get; set; }
         public bool Favorite { get; set; }
         public string Name { get; set; }
+        public SearchMode NameSearchMode { get; set; } = SearchMode.Normal;
         public string Version { get; set; }
+        public SearchMode VersionSearchMode { get; set; } = SearchMode.Normal;
         public StringFilterItemProperties ReleaseYear { get; set; }
         public IdItemFilterItemProperties Genre { get; set; }
         public IdItemFilterItemProperties Platform { get; set; }
