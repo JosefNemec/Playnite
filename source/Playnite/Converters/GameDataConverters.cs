ddab53e1bf47f231223e3709d15f6b35cb10f094
Josef Nemec <nemec.git@fastmail.com>
2026-08-19 14:31:11 +0200

    SDK: Added LibraryObjectNameMatchToBoolConverter converter

---
+++ source/Playnite/Converters/GameDataConverters.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using Playnite.Database;
+using Playnite.SDK.Models;
+
+namespace Playnite.Converters
+{
+    public class LibraryObjectNameMatchToBoolConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var match = false;
+            if (parameter is not string compareName)
+            {
+                match = false;
+            }
+            else
+            {
+                if (value is DatabaseObject obj)
+                    match = GameFieldComparer.StringEquals(obj.Name, compareName);
+
+                else if (value is IEnumerable<DatabaseObject> enumerable)
+                    match = enumerable.Any(a => GameFieldComparer.StringEquals(a.Name, compareName));
+            }
+
+            if (targetType == typeof(Visibility))
+                return match ? Visibility.Visible : Visibility.Collapsed;
+
+            return match;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new NotSupportedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
---
+++ source/Tests/Playnite.Tests/LibraryObjectNameMatchToBoolConverterTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Windows;
+using NUnit.Framework;
+using Playnite.Common;
+using Playnite.Converters;
+using Playnite.Database;
+using Playnite.SDK;
+using Playnite.SDK.Models;
+using Playnite.Tests.Database;
+
+namespace Playnite.Tests
+{
+    [TestFixture]
+    public class LibraryObjectNameMatchToBoolConverterTests
+    {
+        [Test]
+        public void ConvertTests()
+        {
+            using var temp = TempDirectory.Create();
+            using var db = new TestGameDatabase(temp.TempPath);
+            db.OpenDatabase();
+            db.ImportGame(new GameMetadata
+            {
+                Platforms = new HashSet<MetadataProperty>
+                {
+                    new MetadataNameProperty("Test Platform"),
+                    new MetadataNameProperty("Test Platform 2"),
+                },
+                Source = new MetadataNameProperty("Test Source"),
+            }, Guid.Empty);
+
+            var game = db.Games.First();
+            var converter = new LibraryObjectNameMatchToBoolConverter();
+            var convertedVal = converter.Convert(game.Source, typeof(bool), "test-source" , CultureInfo.CurrentCulture);
+            Assert.IsTrue(convertedVal is true);
+
+            convertedVal = converter.Convert(game.Platforms, typeof(bool), "TestPlatform2" , CultureInfo.CurrentCulture);
+            Assert.IsTrue(convertedVal is true);
+
+            convertedVal = converter.Convert(game.Platforms, typeof(Visibility), "TestPlatform2" , CultureInfo.CurrentCulture);
+            Assert.IsTrue(convertedVal is Visibility.Visible);
+
+            convertedVal = converter.Convert(game.Platforms, typeof(bool), "notplatform" , CultureInfo.CurrentCulture);
+            Assert.IsTrue(convertedVal is false);
+
+            convertedVal = converter.Convert(game.Genres, typeof(bool), "notplatform" , CultureInfo.CurrentCulture);
+            Assert.IsTrue(convertedVal is false);
+        }
+    }
+}
