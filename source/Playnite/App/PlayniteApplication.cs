becb65a5f4eb426d5870d3e7554ea4baae491e3f
Josef Nemec <nemec.git@fastmail.com>
2026-09-11 16:03:45 +0200

    Additional tweaks to broken install detection

--- source/Playnite/App/PlayniteApplication.cs
+++ source/Playnite/App/PlayniteApplication.cs
@@ -575,7 +575,10 @@ namespace Playnite
                     // There are various (mostly UnauthorizedAccessException) exceptions when something prevents WPF writing into its temp dir
                     exception.Message.Contains(@"Local\Temp\WPF") ||
                     // This is different CefSharp load error tied specifically to FileNotFoundException exception
-                    (exception is FileNotFoundException && exception.Message.Contains("CefSharp") || exception.Message.Contains("BrowserSubprocessPath")))
+                    (exception is FileNotFoundException &&
+                        (exception.Message.Contains("CefSharp") || exception.Message.Contains("BrowserSubprocessPath") ||
+                         // This is to handle missing .NET assemblies
+                         exception.Message.Contains("PublicKeyToken"))))
                 {
                     Dialogs.ShowErrorMessage("System issue or corrupted Playnite install detected.");
                     Process.GetCurrentProcess().Kill();
